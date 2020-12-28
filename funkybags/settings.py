e8af390709a402542345474e14c519d5286d89d3
hg242322 <68965388+hg242322@users.noreply.github.com>
2020-12-29 01:08:10 +0530

    Update settings.py

--- funkybags/settings.py
+++ funkybags/settings.py
@@ -141,5 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gmala0633@gmail.com'
-EMAIL_HOST_PASSWORD = 'applet123'
+#EMAIL_HOST_USER = 'CONFIDENTIAL'
+#EMAIL_HOST_PASSWORD = 'CONFIDENTIAL'
+# I am sorry, but I cannot disclose my email id and password...I hope you understand...
